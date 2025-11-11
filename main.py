493dd81b06e49d1ff960e76f987ca90c545842d3
Alexander <129068286+idutvuk@users.noreply.github.com>
2025-11-11 22:31:14 +0300

    precommit test 2

---
+++ main.py
@@ -0,0 +1,26 @@
+from functools import lru_cache # unused import
+
+
+print('bad hello world')
+
+
+
+
+
+
+
+def func():
+    return True
+    return False
+
+
+
+
+#bad formatting file example
+
+
+
+z = False
+print(z[0])
+
+
