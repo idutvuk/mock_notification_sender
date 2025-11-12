cde81d48c8e7be2ab51995ba53fecf480e0f2665
Alexander <129068286+idutvuk@users.noreply.github.com>
2025-11-12 12:01:58 +0300

    add factory, cleanup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI, HTTPException
-from notifiers import EmailNotificatior, SmsNotificatior, TelegramNotificatior
+from notifiers import EmailNotificator, SmsNotificator, TelegramNotificator
+import random
 
 app = FastAPI(docs_url="/docs")
 
@@ -11,17 +12,19 @@ def healthcheck():
 
 
 @app.post("/notify", status_code=200)
-def notify_user(user_id: str, message: str):
-    notification_methods = (
-        EmailNotificatior,
-        SmsNotificatior,
-        TelegramNotificatior,
-    )  # assume we dont need affiliation check
-    for notifier in notification_methods:
-        if notifier.notify(user_id, message):
+async def notify_user(user_id: str, message: str):
+    notififiers = (
+        EmailNotificator(),
+        SmsNotificator(),
+        TelegramNotificator(),
+    )
+    for notifier in notififiers:
+        response = await notifier.send_message(user_id, message)
+        if response:
             return user_id
     raise HTTPException(status_code=500, detail="all notificators failed")
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8004, reload=True)
+    random.seed(42)
+    uvicorn.run("main:app", port=8088, reload=True)
--- notifiers.py
+++ notifiers.py
@@ -1,42 +1,58 @@
+import asyncio
 from abc import ABC, abstractmethod
 from loguru import logger
 from random import random
 
 
-class Notificatior(ABC):
-    @staticmethod
+class BaseNotificator(ABC):
+    def __init__(self, fail_rate: float = 0.3, max_delay: float = 1):
+        self.fail_rate = fail_rate
+        self.max_delay = max_delay
+
     @abstractmethod
-    def notify(user_id: str, message: str) -> bool:
+    def send_message(self, user_id: str, message: str) -> bool:
         """abstract function for mocking email, sms, telegram, takes user_id and message - returns bool - send status
         Simulating imperfect connection by random.random()"""
         ...
 
-
-class EmailNotificatior(Notificatior):
-    @staticmethod
-    def notify(user_id: str, message: str) -> bool:
-        if random() < 0.7:  # we have very bad email servers
-            logger.error("Email seervice is unavailable")
+    async def _simulate_network(self) -> bool:
+        await asyncio.sleep(
+            random() * self.max_delay
+        )  # wait for the packet to be transmitted
+        if not random() > self.fail_rate:
+            logger.error(f"{self.__class__.__name__} failed")
             return False
-        logger.debug(f"Sent email notification for {user_id}, {message}")
         return True
 
 
-class SmsNotificatior(Notificatior):
-    @staticmethod
-    def notify(user_id: str, message: str) -> bool:
-        if random() < 0.4:  # we have very bad sms servers
-            logger.error("Sms seervice is unavailable")
-            return False
-        logger.debug(f"Sent sms notification for {user_id}, {message}")
-        return True
+class EmailNotificator(BaseNotificator):
+    def __init__(self):
+        super().__init__(fail_rate=0.7, max_delay=5)
+
+    async def send_message(self, user_id: str, message: str) -> bool:
+        result = await self._simulate_network()
+        if result:
+            logger.debug(f"Sent email notification for {user_id}, {message}")
+        return result
 
 
-class TelegramNotificatior(Notificatior):
-    @staticmethod
-    def notify(user_id: str, message: str) -> bool:
-        if random() < 0.4:  # telegram is blocked again :((
-            logger.error("Telegram seervice is unavailable")
-            return False
-        logger.debug(f"Sent telegram notification for {user_id}, {message}")
-        return True
+class SmsNotificator(BaseNotificator):
+    def __init__(self):
+        super().__init__(fail_rate=0.5, max_delay=3)
+
+    async def send_message(self, user_id: str, message: str) -> bool:
+        result = await self._simulate_network()
+        if result:
+            logger.debug(f"Sent sms notification for {user_id}, {message}")
+        return result
+
+
+class TelegramNotificator(BaseNotificator):
+    def __init__(self):
+        super().__init__(fail_rate=0.5, max_delay=1)
+
+    async def send_message(self, user_id: str, message: str) -> bool:
+        result = await self._simulate_network()
+        if result:
+            logger.debug(f"Sent tg notification for {user_id}, {message}")
+        return result
