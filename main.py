3f2e0da459d79a2e67f340575fd91c5192067c44
Alexander <129068286+idutvuk@users.noreply.github.com>
2025-11-12 12:42:31 +0300

    add retries

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import uvicorn
 from fastapi import FastAPI, HTTPException
-from notifiers import EmailNotificator, SmsNotificator, TelegramNotificator
-import random
+from notifiers import EmailNotificator, SmsNotificator, TelegramNotificator, Message
 
-app = FastAPI(docs_url="/docs")
+app = FastAPI(docs_url="/")
 
 
 @app.get("/health")
@@ -12,19 +11,18 @@ def healthcheck():
 
 
 @app.post("/notify", status_code=200)
-async def notify_user(user_id: str, message: str):
+async def notify_user(message: Message):
     notififiers = (
         EmailNotificator(),
         SmsNotificator(),
         TelegramNotificator(),
     )
     for notifier in notififiers:
-        response = await notifier.send_message(user_id, message)
+        response = await notifier.send_with_retry(message)
         if response:
-            return user_id
+            return message
     raise HTTPException(status_code=500, detail="all notificators failed")
 
 
 if __name__ == "__main__":
-    random.seed(42)
-    uvicorn.run("main:app", port=8088, reload=True)
+    uvicorn.run("main:app", port=8089, reload=True)
--- notifiers.py
+++ notifiers.py
@@ -1,7 +1,19 @@
 import asyncio
 from abc import ABC, abstractmethod
+from typing import Optional
+
 from loguru import logger
 from random import random
+
+from pydantic import BaseModel
+
+
+class Message(BaseModel):
+    user_id: str
+    title: Optional[str]
+    message: str
+    # def __repr__(self) -> str:
+    #     return f"message for {self.user_id} with title: {self.title[:50]}: {self.message[:50]}"
 
 
 class BaseNotificator(ABC):
@@ -9,8 +21,22 @@ class BaseNotificator(ABC):
         self.fail_rate = fail_rate
         self.max_delay = max_delay
 
+    async def send_with_retry(self, message: Message, max_retries=3) -> bool:
+        result = await self.send_message(message)
+        if result:
+            return True
+        logger.error("failed to send")
+        for i in range(max_retries):
+            logger.error(f"sending retry failed ({i}/{max_retries})")
+            result = await self.send_message(message)
+            if result:
+                return True
+        return False
+
+        # for try_id in range(max_retries):
+
     @abstractmethod
-    def send_message(self, user_id: str, message: str) -> bool:
+    async def send_message(self, message: Message) -> bool:
         """abstract function for mocking email, sms, telegram, takes user_id and message - returns bool - send status
         Simulating imperfect connection by random.random()"""
         ...
@@ -27,32 +53,32 @@ class BaseNotificator(ABC):
 
 class EmailNotificator(BaseNotificator):
     def __init__(self):
-        super().__init__(fail_rate=0.7, max_delay=5)
+        super().__init__(fail_rate=0.9, max_delay=5)
 
-    async def send_message(self, user_id: str, message: str) -> bool:
+    async def send_message(self, message: Message) -> bool:
         result = await self._simulate_network()
         if result:
-            logger.debug(f"Sent email notification for {user_id}, {message}")
+            logger.debug(f"Sent email notification - {message}")
         return result
 
 
 class SmsNotificator(BaseNotificator):
     def __init__(self):
-        super().__init__(fail_rate=0.5, max_delay=3)
+        super().__init__(fail_rate=0.6, max_delay=3)
 
-    async def send_message(self, user_id: str, message: str) -> bool:
+    async def send_message(self, message: Message) -> bool:
         result = await self._simulate_network()
         if result:
-            logger.debug(f"Sent sms notification for {user_id}, {message}")
+            logger.debug(f"Sent sms notification - {message}")
         return result
 
 
 class TelegramNotificator(BaseNotificator):
     def __init__(self):
-        super().__init__(fail_rate=0.5, max_delay=1)
+        super().__init__(fail_rate=0.3, max_delay=2)
 
-    async def send_message(self, user_id: str, message: str) -> bool:
+    async def send_message(self, message: Message) -> bool:
         result = await self._simulate_network()
         if result:
-            logger.debug(f"Sent tg notification for {user_id}, {message}")
+            logger.debug(f"Sent tg notification - {message}")
         return result
